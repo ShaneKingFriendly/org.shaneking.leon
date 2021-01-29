c36dcac975cd2054130b856e200d636a4d18aa1f
ShaneKing <github@shaneking.org>
2021-01-29 22:52:25 +0800

    add test case

---
+++ org.shaneking.leon.swagger/src/test/java/sktest/leon/swagger/cfg/WebSwaggerCfgTest.java
@@ -0,0 +1,13 @@
+package sktest.leon.swagger.cfg;
+
+import org.junit.jupiter.api.Test;
+import org.shaneking.ling.zero.lang.String0;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class WebSwaggerCfgTest {
+  @Test
+  void test() {
+    assertEquals(String0.ARY_DEC, String0.DIGITAL);
+  }
+}
