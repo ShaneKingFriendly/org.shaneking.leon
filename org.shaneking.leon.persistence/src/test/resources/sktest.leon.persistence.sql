5adbeaebcacf6a3a00bdbfce763ae31d800d7e6e
ShaneKing <github@shaneking.org>
2021-03-13 19:17:19 +0800

    adjust maven repo, redirect to oss for travis-ci

--- org.shaneking.leon.persistence/src/test/resources/sktest.leon.persistence.sql
+++ org.shaneking.leon.persistence/src/test/resources/sktest.leon.persistence.sql
@@ -118,7 +118,7 @@ select 0,'1612262610215_LoHqeZBGrVYm3MlYmpH','N','','1612262610216_koFVLCNZrhezb
 insert into t_hello_user_entity (version, id, invalid, last_modify_date_time, last_modify_user_id, tenant_id, no, name, haha, mobile, email)
 select 0,'1612262610216_koFVLCNZrhezbgULWqW','N','','1612262610216_koFVLCNZrhezbgULWqW','1612262610215_LoHqeZBGrVYm3MlYmpH','tstUserNo','tstUserName','[SKC1]494c6f7665596f75','18888888888','email@email.com';
 insert into t_hello_channel_entity (version, id, invalid, last_modify_date_time, last_modify_user_id, no, name, description, token_value, token_force, token_algorithm_type, token_value_type)
-select 0,'1612263653223_oGFvE5Hyndf0njoFhyK','N','','1612262610216_koFVLCNZrhezbgULWqW','tstChannelNo','tstChannelName','tstChannelDesc','494c6f7665596f75','N','C3','SELF';
+select 0,'1612263653223_oGFvE5Hyndf0njoFhyK','N','','1612262610216_koFVLCNZrhezbgULWqW','tstChannelNo','tstChannelName','tstChannelDesc','494c6f7665596f75','N','SKC1','SELF';
 insert into t_hello_api_access_entity (version, id, invalid, last_modify_date_time, last_modify_user_id, tenant_id, channel_id, allow_url_regex, allow_signature_regex, deny_url_regex, deny_signature_regex)
 select 0,'1612263668482_jaHu6tmguyKo2xWgHPj','N','','1612262610216_koFVLCNZrhezbgULWqW','1612262610215_LoHqeZBGrVYm3MlYmpH','1612263653223_oGFvE5Hyndf0njoFhyK','','^[\s\S]*(add|delById|mod|mge|lst)\([\s\S]*$','','';
 
