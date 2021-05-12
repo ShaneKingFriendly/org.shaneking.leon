746810cd5b6eb837398e3cb16f55c8822e8d8a5e
ShaneKing <github@shaneking.org>
2021-05-12 22:57:32 +0800

    0.241.6.Friendly-SNAPSHOT

--- org.shaneking.leon.rr/src/main/java/org/shaneking/leon/rr/ac/package-info.java
+++ org.shaneking.leon.rr/src/main/java/org/shaneking/leon/rr/ac/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Access Control
  * <p>
- * ctl(if can't control, like file controller) -> ac -> biz
- * rpc(can't control, so need ac, like dubbo) -> ac -> biz
+ * ctl(if can't control, like file controller) -7 ac -7 biz
+ * rpc(can't control, so need ac, like dubbo) -7 ac -7 biz
  */
 package org.shaneking.leon.rr.ac;
