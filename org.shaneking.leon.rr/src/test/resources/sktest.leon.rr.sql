cbb030cf0695c55c2daf00b43dd5611d2dd453a6
ShaneKing <github@shaneking.org>
2022-09-10 18:49:53 +0800

    adjust channel api access

--- org.shaneking.leon.rr/src/test/resources/sktest.leon.rr.sql
+++ org.shaneking.leon.rr/src/test/resources/sktest.leon.rr.sql
@@ -261,7 +261,7 @@ select '1612262610216_koFVLCNZrhezbgULWqW','tstUserNo','','1612262610216_koFVLCN
 insert into t_simple_channel_entity (id, no, lm_dsz, lm_uid, name, description, enc_tv, enc_tf, enc_tat, enc_tvt)
 select '1612263653223_oGFvE5Hyndf0njoFhyK','tstChannelNo','','1612262610216_koFVLCNZrhezbgULWqW','tstChannelName','tstChannelDesc','494c6f7665596f75','N','SKC1','SELF';
 insert into t_simple_channel_access_regex_api_entity (id, no, lm_dsz, lm_uid, channel_id, allow_url_regex, allow_signature_regex, deny_url_regex, deny_signature_regex)
-select '1612263668482_jaHu6tmguyKo2xWgHPj','','','1612262610216_koFVLCNZrhezbgULWqW','1612263653223_oGFvE5Hyndf0njoFhyK','^[\s\S]*(user/rmvById|user/mge)[\s\S]*$','^[\s\S]*(add|lst)\([\s\S]*$','','';
+select '1612263668482_jaHu6tmguyKo2xWgHPj','','','1612262610216_koFVLCNZrhezbgULWqW','1612263653223_oGFvE5Hyndf0njoFhyK','^[\s\S]*$','^[\s\S]*(add|lst)\([\s\S]*$','','';
 
 
 select * from t_simple_channel_access_regex_api_entity;
